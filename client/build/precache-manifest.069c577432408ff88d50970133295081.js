2c69673fa84e47d480b3744018f4d1b8544645d7
Kathy Erdmann <kathryn.erdmann@hsc.utah.edu>
2019-01-01 19:11:32 -0700

    trying to deploy

---
+++ client/build/precache-manifest.069c577432408ff88d50970133295081.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "70ee416ed4187abeda0d9d71740658f0",
+    "url": "/nyt-react/static/media/Newspapers1.70ee416e.jpg"
+  },
+  {
+    "revision": "dd758aa3fb4cf82333cb",
+    "url": "/nyt-react/static/js/runtime~main.dd758aa3.js"
+  },
+  {
+    "revision": "2939446a7c901cf4e450",
+    "url": "/nyt-react/static/js/main.2939446a.chunk.js"
+  },
+  {
+    "revision": "ae120c13639f55661941",
+    "url": "/nyt-react/static/js/1.ae120c13.chunk.js"
+  },
+  {
+    "revision": "2939446a7c901cf4e450",
+    "url": "/nyt-react/static/css/main.3670f7ac.chunk.css"
+  },
+  {
+    "revision": "7c15a41b3d67e2359a3dcf11c903026a",
+    "url": "/nyt-react/index.html"
+  }
+];
